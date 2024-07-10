8911dfcb0a40d99f0b76454136033fa870c5270a
Ovsep <bozojan0505@mail.ru>
2024-07-10 21:42:11 +0700

    bro nu kak?

---
+++ bombaproj/decor.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void BestIntroduction()
+{
+    printf("                                                                 00     00                          !!!!    \n");
+    printf("     CCCCCCCCCC              AAA            ооооооооооооооо    EEEEEEEEEEEEEE    PPPPPPPPPPPPP      !!!!    \n");
+    printf("   CCCCCCCCCCCCCC           AAAAA           ооооооооооооооо    EEEEEEEEEEEEEE    PPP        PPP     !!!!    \n");
+    printf("  CCC          CCC         AA   AA          ооо         ооо    EEE               PPP          PP    !!!!    \n");
+    printf("  CC                      AA     AA         ооо         ооо    EEE               PPP          PP    !!!!    \n");
+    printf("  CC                     AA       AA        ооо         ооо    EEEEEEEEEE        PPP        PPP     !!!!    \n");
+    printf("  CC                    AA         AA       ооо         ооо    EEE               PPPPPPPPPPPP       !!!!    \n");
+    printf("  CCC          CCC     AAAAAAAAAAAAAAA      ооо         ооо    EEE               PPP                        \n");
+    printf("   CCCCCCCCCCCCCC     AAAA         AAAA     ооо         ооо    EEEEEEEEEEEEEE    PPP                !!!!    \n");
+    printf("     CCCCCCCCCC      AAAA           AAAA    ооо         ооо    EEEEEEEEEEEEEE    PPP                !!!!    \n");
+    printf("\n\n\n");
+
+}
