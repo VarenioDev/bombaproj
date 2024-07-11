915713573938654c67232571c7207922e1afb633
Ovsep <bozojan0505@mail.ru>
2024-07-11 23:08:17 +0700

    Реализвал main через switch

--- bombaproj/bombaproj.cpp
+++ bombaproj/bombaproj.cpp
@@ -15,13 +15,7 @@
 using namespace std;
 
 int TOTAL_BOMBS, ROWS, COLUMNS;
-
-int getRandomNumber(int min, int max) { // что за неописуемая суета!!???
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> distr(min, max);
-    return distr(gen);
-}
+OutputManager manager;
 
 void DifficultyChoose(int choose) {
     system("cls");
@@ -45,6 +39,13 @@ void DifficultyChoose(int choose) {
         cout << "ТЫ ДЕБИЛ????? Запускай заново!";
         break;
     }
+}
+
+int getRandomNumber(int min, int max) { // что за неописуемая суета!!???
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> distr(min, max);
+    return distr(gen);
 }
 
 void SetColor(int textColor, int bgColor) {
@@ -119,12 +120,100 @@ vector<vector<cell>> InitializeMap() {
 
 int main()
 {
-    OutputManager manager;
-
     setlocale(LC_ALL, "Russian");
     srand(time(0));
 
     manager.BestIntroduction();
+    manager.MainMenu();
+
+    int select = _getch();
+    switch(select){
+    case 1:
+    {
+        manager.DifficultyOffer();
+        int choose = _getch();
+        DifficultyChoose(choose);
+
+        vector<vector<cell>> cells = InitializeMap();
+        manager.SendCounter(TOTAL_BOMBS);
+        manager.SendRules();
+
+        std::chrono::steady_clock::time_point startTime;
+        StartTimer(startTime);
+        Frame frame(0, 0);
+        while (true) {
+
+            int arg = _getch();
+
+            if (arg == 0 || arg == 224) {
+                arg = _getch();
+                switch (arg) {
+                case 72:
+                    if (frame.y > 0) frame.y -= 1;
+                    break;
+                case 80:
+                    if (frame.y < ROWS - 1) frame.y += 1;
+                    break;
+                case 75:
+                    if (frame.x > 0) frame.x -= 1;
+                    break;
+                case 77:
+                    if (frame.x < COLUMNS - 1) frame.x += 1;
+                    break;
+                }
+            }
+            else if (arg == 102) {
+                if (!cells[frame.y][frame.x].IsShowed) cells[frame.y][frame.x].IsFlagged = !cells[frame.y][frame.x].IsFlagged;
+            }
+            else if (arg == 32) {
+                int a = frame.y;
+                int b = frame.x;
+                cells[a][b].IsShowed = true;
+                ShowAround(cells, a, b);
+            }
+            system("cls");
+
+            int totalShowed = 0;
+            int totalFlags = TOTAL_BOMBS;
+
+            for (int i = 0; i < ROWS; i++) {
+                cout << "       ";
+                for (int j = 0; j < COLUMNS; j++) {
+                    if (i == frame.y && j == frame.x) {
+                        cells[i][j].IsUnderCursor = true;
+                        cells[i][j].Show();
+                        cells[i][j].IsUnderCursor = false;
+                    }
+                    else cells[i][j].Show();
+                    totalShowed += cells[i][j].IsShowed;
+                    totalFlags -= cells[i][j].IsFlagged;
+                }
+                cout << "\n";
+            }
+
+            manager.SendCounter(totalFlags);
+            manager.SendRules();
+
+            if (ROWS * COLUMNS - totalShowed == TOTAL_BOMBS) {
+                cout << "\nВы выиграли!\n" << StopTimer(startTime);
+                break;
+            }
+
+            if (cells[frame.y][frame.x].IsBomb && cells[frame.y][frame.x].IsShowed) {
+                cout << "\nВы проиграли!\n" << StopTimer(startTime);
+                break;
+            }
+        }
+    }
+        break;
+    case 2:
+        return 0;
+        
+    case 3:
+        manager.BestIntroduction();
+        cout << "Спасибо, что поиграли в нашу игру!<3";
+        return 0;
+    }
     manager.DifficultyOffer();
     int select = _getch();
     DifficultyChoose(select);
