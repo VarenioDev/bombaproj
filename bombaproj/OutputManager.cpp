b8b8f03de4b04119ccb74ec088f494dd85f74879
Ovsep <bozojan0505@mail.ru>
2024-07-11 10:57:16 +0700

    Written Output Texts

--- bombaproj/OutputManager.cpp
+++ bombaproj/OutputManager.cpp
@@ -1,15 +1,10 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 class OutputManager {
 public:
-	static void SendCounter(int flags) {
-		cout << "------------------------ \n| Осталось мин: " << flags << "      | \n------------------------\n";
-	}
-	static void SendRules() {
-		cout << "------------------------ \n| Управление: \n| Стрелочки - перемещение по полю \n| f - установка флага \n| Пробел - вскопать поле \n------------------------";
-	}
-
+    //1 - intro
     static void BestIntroduction()
     {
         printf("                                                                 00     00                          !!!!    \n");
@@ -25,5 +20,23 @@ public:
         printf("\n\n\n");
 
     }
+    //2 - main menu
+    static void MainMenu() {}
+    //2.1 - create game
+    static void DifficultyOffer() {}
+
+	static void SendCounter(int flags) {
+		cout << "------------------------ \n| Осталось мин: " << flags << "      | \n------------------------\n";
+	}
+	static void SendRules() {
+		cout << "------------------------ \n| Управление: \n| Стрелочки - перемещение по полю \n| f - установка флага \n| Пробел - вскопать поле \n------------------------";
+	}
+
+    //2.2 - leaderboard
+    static void LeaderBoard(/*работа с файлом*/) {}
+
+    //2.3 - meme(if no deadline)
+
+    
 };
 
