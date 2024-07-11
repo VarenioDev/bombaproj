ab10880d10582561af065c9837a4e8f30ea148ca
Ovsep <bozojan0505@mail.ru>
2024-07-11 09:31:07 +0700

    function BestIntroduction() was added in OutputMannager.cpp

--- bombaproj/OutputManager.cpp
+++ bombaproj/OutputManager.cpp
@@ -9,4 +9,21 @@ public:
 	static void SendRules() {
 		cout << "------------------------ \n| Óïðàâëåíèå: \n| Ñòðåëî÷êè - ïåðåìåùåíèå ïî ïîëþ \n| f - óñòàíîâêà ôëàãà \n| Ïðîáåë - âñêîïàòü ïîëå \n------------------------";
 	}
+
+    static void BestIntroduction()
+    {
+        printf("                                                                 00     00                          !!!!    \n");
+        printf("     CCCCCCCCCC              AAA            ÏÏÏÏÏÏÏÏÏÏÏÏÏÏÏ    EEEEEEEEEEEEEE    PPPPPPPPPPPPP      !!!!    \n");
+        printf("   CCCCCCCCCCCCCC           AAAAA           ÏÏÏÏÏÏÏÏÏÏÏÏÏÏÏ    EEEEEEEEEEEEEE    PPP        PPP     !!!!    \n");
+        printf("  CCC          CCC         AA   AA          ÏÏÏ         ÏÏÏ    EEE               PPP          PP    !!!!    \n");
+        printf("  CC                      AA     AA         ÏÏÏ         ÏÏÏ    EEE               PPP          PP    !!!!    \n");
+        printf("  CC                     AA       AA        ÏÏÏ         ÏÏÏ    EEEEEEEEEE        PPP        PPP     !!!!    \n");
+        printf("  CC                    AA         AA       ÏÏÏ         ÏÏÏ    EEE               PPPPPPPPPPPP       !!!!    \n");
+        printf("  CCC          CCC     AAAAAAAAAAAAAAA      ÏÏÏ         ÏÏÏ    EEE               PPP                        \n");
+        printf("   CCCCCCCCCCCCCC     AAAA         AAAA     ÏÏÏ         ÏÏÏ    EEEEEEEEEEEEEE    PPP                !!!!    \n");
+        printf("     CCCCCCCCCC      AAAA           AAAA    ÏÏÏ         ÏÏÏ    EEEEEEEEEEEEEE    PPP                !!!!    \n");
+        printf("\n\n\n");
+
+    }
 };
+
